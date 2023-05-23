23d4235a3b08ec1315a0faf2981e214a72c3f65e
josedurigon <jose.durigon@outlook.com>
2023-05-23 12:52:40 -0300

    2°

--- app/src/main/java/com/example/segundoapp/MainActivity.kt
+++ app/src/main/java/com/example/segundoapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.segundoapp
 
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,6 +10,8 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.segundoapp.databinding.ActivityMainBinding
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.*
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -17,28 +20,38 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.maps.android.heatmaps.HeatmapTileProvider
 
 
-class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: DatabaseReference
-
+    private lateinit var googleMap: GoogleMap
 
     val db = FirebaseFirestore.getInstance()
-    var handler = Handler()
+    var handler = Handler(Looper.getMainLooper())
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-     binding = ActivityMainBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapViewContainer) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
 
-        setSupportActionBar(binding.toolbar)
-
-        }
+    override fun onMapReady(googleMap: GoogleMap) {
+        val sydney = LatLng(-33.852, 151.211)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+    }
 
     fun consulta(){
         val collectionRef = db.collection("IntegrasData")
@@ -50,16 +63,13 @@ class MainActivity : AppCompatActivity() {
                     // Lógica para manipular os dados
                 }
             }
+
             .addOnFailureListener { exception ->
+                "Erro ao tentar consultar o banco de dados na coleção ${collectionRef}"
                 // Lógica para lidar com erros
             }
 
     }
-
-
-
-
-
 
 
 }
