079bb8b414596d8d3f80be2788b4c341f33e4527
josedurigon <jose.durigon@outlook.com>
2023-05-23 15:58:04 -0300

    2°

--- app/src/main/java/com/example/segundoapp/MainActivity.kt
+++ app/src/main/java/com/example/segundoapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.segundoapp
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.Manifest
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -28,6 +32,10 @@ import com.google.maps.android.heatmaps.HeatmapTileProvider
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    companion object {
+        private const val REQUEST_CODE = 123 // Valor
+    }
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: DatabaseReference
     private lateinit var googleMap: GoogleMap
@@ -40,8 +48,25 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Permissão já foi concedida
+            // Coloque aqui o código para usar a localização
+        } else {
+            // Permissão ainda não foi concedida, solicite-a ao usuário
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
+        }
+
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapViewContainer) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
+        if (mapFragment == null) {
+            val newMapFragment = SupportMapFragment.newInstance()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.mapViewContainer, newMapFragment)
+                .commit()
+            newMapFragment.getMapAsync(this)
+        } else {
+            mapFragment.getMapAsync(this)
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
